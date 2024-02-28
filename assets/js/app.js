80bae1cc949dd07965ca7d2fb4dd5ce5d2ea9d6a
rasmussoender <rasmuspedersen14@gmail.com>
2024-02-28 12:27:55 +0100

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,35 @@
 
 // First - store a value using sessionStorage.setItem("key", "value")
 
+sessionStorage.setItem("test", "Disneyland");
+
 // Now get it and display it somewhere
+const someValue = sessionStorage.getItem("test")
 
 // Delete the sessionStorage and the code above and do the main task:
 
 // Get the form
+const formEl = document.querySelector("form");
 
 // Listen for submit event, prevent default, store the first+last name in sessionStorage
 
+if(formEl){
+    
+    formEl.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const firstName = formEl.formFirstName.value;
+        
+        const lastName = formEl.formLastName.value;
+        
+        sessionStorage.setItem("username", `${firstName} ${lastName}`);
+        currentuserEl.textContent = sessionStorage.getItem("username");
+        
+    });
+}
+    
 // Get the .currentUser and store the value of the sessionStorage in there
+const currentuserEl = document.querySelector(".currentUser")
+currentuserEl.textContent = sessionStorage.getItem("username");
 
 // Now try getting the sessionStorage from anotherfile.html
 
